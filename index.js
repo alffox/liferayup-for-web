2f9061e341c912f24a1d1bb249c509894f8b591a
alffox <alfonso.crisci.it@gmail.com>
2019-01-29 12:20:28 +0100

    Revert "chore: Add node.js scripts the page to be served"
    This reverts commit 40da53ec7f36b017d01f31eaef38ff0b910d90e1.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const express = require("express");
-const app = express();
-const path = require('path');
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(3000, function(){
+    console.log('Server running on 3000...');
 });
-
-app.listen(3000, () => console.log("Server listening on port 3000!"));
