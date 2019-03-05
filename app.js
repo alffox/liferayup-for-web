a0e0e7941e4d52d65f809480754fc302c726046d
alffox <alfonso.crisci.it@gmail.com>
2019-03-05 10:07:22 +0100

    refactor: POC to fetch Liferay versions

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+var liferayData ;
 var xhr = new XMLHttpRequest();
 xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
-        console.log(JSON.parse(xhr.responseText));
-    } else {
-        console.log(JSON.parse(xhr.responseText));
+        liferayData = JSON.parse(xhr.responseText);
+        console.log(liferayData["service-packs"]);
+    } else {//throw error
+        console.log("There was an error fetching the data");
     }
-
 };
 
 xhr.open('GET', './info.json'); // This file is the local json copy of https://files.liferay.com/public/support/patching-tool-info.xml
