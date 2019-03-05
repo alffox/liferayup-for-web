d209636d54520b966531257cc238260e85953b98
alffox <alfonso.crisci.it@gmail.com>
2019-03-05 10:20:39 +0100

    refactor: Switch to MVC pattern

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var liferayData ;
-var xhr = new XMLHttpRequest();
-xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        liferayData = JSON.parse(xhr.responseText);
-        console.log(liferayData["service-packs"]);
-    } else {//throw error
-        console.log("There was an error fetching the data");
-    }
-};
+var model = {};//Load data locally or from a remote server
 
-xhr.open('GET', './info.json'); // This file is the local json copy of https://files.liferay.com/public/support/patching-tool-info.xml
-xhr.send();
+var commander = {
+    initData: function () {
+        commander.getLiferayVersions();
+    },
+
+    getLiferayVersions: function () {
+        var liferayVersions;
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                liferayVersions = JSON.parse(xhr.responseText);
+                console.log(liferayVersions["service-packs"]);
+            } else {//throw error
+                console.log("There was an error fetching the data");
+            }
+        };
+
+        xhr.open('GET', './info.json'); // This file is the local json copy of https://files.liferay.com/public/support/patching-tool-info.xml
+        xhr.send();
+    };
+
+},
+
+var view = {};
