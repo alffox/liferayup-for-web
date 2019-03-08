c462c5934d3b1a6377e49bf5c79aa42af0613672
alffox <alfonso.crisci.it@gmail.com>
2019-03-08 10:12:27 +0100

    style: Reorganizing comments

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var liferayData;
+var liferayData;// Initialize empty variable. These data will be fetched once to prevent duplicate calls and reused in the app
 
-var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();// Fetch data asynchronously with this plain XMLHttpRequest to preserve IE11 compatibility 
 xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
-        liferayData = JSON.parse(xhr.responseText);
+        liferayData = JSON.parse(xhr.responseText);// Store data globally
 
         commander.initData(liferayData);
     } else {//throw error
@@ -25,14 +25,13 @@ var commander = {// Actual logic
 
         liferayVersions = [];// Initializes empty versions array
 
-        // Populates liferayVersions empty array with value equal or major than 6.2
         for (var i = 1; i < liferayData["service-packs"].length; i++) {
-            liferayVersions.push(liferayData["service-packs"][i].version);
+            liferayVersions.push(liferayData["service-packs"][i].version);// Populates liferayVersions empty array with 6.2+ Liferay versions
         }
         view.renderLiferayVersions(liferayVersions);// Delegate the rendering
     },
 
-    getLiferayVersion: function () {
+    getLiferayVersion: function () {// Listen to change event on Liferay version select fields and get the value upon choosing
         document.querySelector('select[id="liferayVersionFormSelect"]').onchange = function (event) {
             alert(event.target.value);
         }
@@ -51,7 +50,7 @@ var view = {// Render data on the interface
             $liferayVersionFormSelect.appendChild(optionNode);
             optionNode.textContent = liferayVersions[i];
         }
-        commander.getLiferayVersion();
+        commander.getLiferayVersion();// Correct values stored in DOM, now let's call commander.getLiferayVersion() method to bind change listener event and get the selected data
     }
 
 };
